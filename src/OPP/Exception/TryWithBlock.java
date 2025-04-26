1048439a8f358203bafd0c9acfe21371c01e5a7a
shivam <edge07.shiv@gmail.com>
2025-04-26 16:37:16 +0530

    Learning try-with

---
+++ src/OPP/Exception/TryWithBlock.java
@@ -0,0 +1,25 @@
+package OPP.Exception;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithBlock {
+    public static void main(String[] args) {
+        try(BufferedReader reader = new BufferedReader(new FileReader("example.txt"))) {
+            boolean line;
+            while ((line = reader.readLine() != null)) {
+                System.out.println(line);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Exception caught");
+        }
+    }
+}
+
+// this is just notes, at this time while I am studying this topic I am not aware about file handling
+
+// try-with-resources
+// the object that is being created in 'try(---)'  parenthesis will be closed automatically if the class of the object is implementing the AutoCloseable interface
+// no need of finally block to close the file
